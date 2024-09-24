f2af85fcfff6444015584a4d5d8eb57b859eca1d
ayaA101 <fuji0165175@gmail.com>
2024-09-24 18:43:38 +0900

    バリデーションの記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,22 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :items
+  has_many :orders, through: :items
+
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  PASSWORD_REGEX_ZENKAKU = /\A[ぁ-んァ-ン一-龥]+\z/.freeze
+  PASSWORD_REGEX_KATAKANA = /\A[ァ-ヶー－]+\z/.freeze
+
+  with_options presence: true do
+    validates :nickname
+    validates :email
+    validates :encrypted_password, format: { with: PASSWORD_REGEX }
+    validates :last_name, format: { with: PASSWORD_REGEX_ZENKAKU }
+    validates :first_name, format: { with: PASSWORD_REGEX_ZENKAKU }
+    validates :last_name_kana, format: { with: PASSWORD_REGEX_KATAKANA }
+    validates :first_name_kana, format: { with: PASSWORD_REGEX_KATAKANA }
+    validates :birth_date
+  end
 end
