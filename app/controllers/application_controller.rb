f81d7d86eee222d4f6a869cccfd627b479ab42cc
ayaA101 <fuji0165175@gmail.com>
2024-09-24 18:33:28 +0900

    新規登録ログイン機能の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,13 @@ class ApplicationController < ActionController::Base
   before_action :basic_auth
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  private
+  protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :last_name, :first_name, :last_name_kana, :first_name_kana, :birth_date])
   end
+
+  private
 
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
