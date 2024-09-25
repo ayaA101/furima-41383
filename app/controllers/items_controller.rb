889f11373bc048d7e8847ef16d923e8d962845d9
ayaA101 <fuji0165175@gmail.com>
2024-09-25 15:09:11 +0900

    ルート記述/new, create

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,23 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :image, :price, :text, :user_id, :genre_id, :quality_id, :payment_id, :prefecture_id, :days_id).merge(user_id: current_user.id)
+  end
 end
