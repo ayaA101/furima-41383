7e87fa0e51cd536030b089cc41d11244f703cced
ayaA101 <fuji0165175@gmail.com>
2024-10-04 18:11:32 +0900

    商品出品機能単体テスト

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,20 @@
 FactoryBot.define do
   factory :item do
+    association :user
+
+    item_name { Faker::Name.initials(number: 2) }
+    item_info { 'あいう' }
+    item_category_id { 2 }
+    item_status_id { 2 }
+    option_id { 2 }
+    prefecture_id { 2 }
+    schedule_id { 2 }
+    price { 500 }
+    user_id { @user }
     
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
+
 end
