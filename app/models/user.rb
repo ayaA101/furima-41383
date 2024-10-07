47ea93e2a520a9d2306d26b98a6a6a37020f7511
ayaA101 <fuji0165175@gmail.com>
2024-10-07 18:32:14 +0900

    オーダーモデル作成
    コントローラー、マイグレーション記述（後の売却済み商品の管理に使う）

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_many :items
+  has_many :orders
 
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
   PASSWORD_REGEX_ZENKAKU = /\A[ぁ-んァ-ヶ一-龥々ー]+\z/.freeze
