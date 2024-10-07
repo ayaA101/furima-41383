c43fe9826a96ccce1886d2331d56c200413b47a3
ayaA101 <fuji0165175@gmail.com>
2024-10-07 19:20:11 +0900

    レビュー後修正２

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -22,8 +22,6 @@ class ItemsController < ApplicationController
   def show
   end
 
-  def edit
-  end
 
   private
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :items
-  has_many :orders
+
 
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
   PASSWORD_REGEX_ZENKAKU = /\A[ぁ-んァ-ヶ一-龥々ー]+\z/.freeze
