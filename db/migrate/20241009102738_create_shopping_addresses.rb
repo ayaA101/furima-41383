84cfd64918047ae077d33b4b1f706b65c276a448
ayaA101 <fuji0165175@gmail.com>
2024-10-09 20:00:23 +0900

    shopping_adressモデルのアソシエーション関連記述

--- db/migrate/20241009102738_create_shopping_addresses.rb
+++ db/migrate/20241009102738_create_shopping_addresses.rb
@@ -1,7 +1,13 @@
 class CreateShoppingAddresses < ActiveRecord::Migration[7.0]
   def change
     create_table :shopping_addresses do |t|
-
+      t.string :postalcode, null: false
+      t.integer :prefecture_id, null: false
+      t.string :city, null: false
+      t.string :addresses, null: false
+      t.string :building
+      t.string :phonenumber, null:false
+      t.references :order, null:false, foreign_key: true
       t.timestamps
     end
   end
