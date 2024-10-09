894577d7b9e2ca086e1de527dc900bd72dcf6f71
ayaA101 <fuji0165175@gmail.com>
2024-10-10 04:07:29 +0900

    エラー文の実装編集

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,18 +1,19 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_order 
+  before_action :set_order, only:[:index, :create]
+
 
   def index
-    @orderrecord = OrderRecord.new
+    @order_record = OrderRecord.new
     if current_user.id == @item.user_id 
       return redirect_to root_path
     end   
   end  
 
   def create
-    @orderrecord = OrderRecord.new(order_params)
-    if @orderrecord.valid?
-       @orderrecord.save(params,current_user.id)
+    @order_record = OrderRecord.new(order_params)
+    if @order_record.valid?
+       @order_record.save(params,current_user.id)
          redirect_to root_path
     else
          render :index, status: :unprocessable_entity
