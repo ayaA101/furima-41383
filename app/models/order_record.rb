f41ad6eb6ea8419f439948e5e60893bc6dce88af
ayaA101 <fuji0165175@gmail.com>
2024-10-09 19:07:52 +0900

    Formオブジェクトの作成

---
+++ app/models/order_record.rb
@@ -0,0 +1,19 @@
+class OrderRecord
+  include ActiveModel::Model
+  attr_accessor :user_id, :item_id, :postalcode, :prefecture_id, :city, :addresses, :building, :phonenumber
+
+  with_options presence: true do
+    validates :user_id
+    validates :item_id
+    validates :postalcode, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :prefecture_id, numericality: {other_than: 1, message: "can't be blank"}
+    validates :city
+    validates :addresses
+    validates :phonenumber, format: { with: /\A\d{10,11}\z/,message: "is invalid. Include hyphen(-)"}
+  end
+
+  def save
+    order = Order.create(user_id: user_id, item_id: item_id)
+    ShoppingAddress.create(postalcode: postalcode, prefecture_id: prefecture_id, city: city, addresses: addresses, building: building, phonenumber: phonenumber, order_id: order.id)
+  end
+end
