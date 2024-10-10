78f69e074cc89649d737a9c9d57e22c72b8ec2ae
ayaA101 <fuji0165175@gmail.com>
2024-10-10 17:49:18 +0900

    アクション記載漏れ編集

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -6,7 +6,7 @@ class OrdersController < ApplicationController
   def index
     gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
     @order_record = OrderRecord.new
-    if current_user.id == @item.user_id 
+    if current_user.id == @item.user_id || @item.order != nil
       return redirect_to root_path
     end   
   end  
